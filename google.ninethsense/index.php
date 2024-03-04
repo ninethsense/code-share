dd1614f1e2a2674853aeb6bd9bae3f20495b6f66
Praveen Nair <ninethsense@outlook.com>
2024-03-04 18:10:12 +0530

    google.ninethsense.com

---
+++ google.ninethsense/index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['c'])) {
+	$_SESSION['c'] = file_get_contents("./counter.txt");
+	file_put_contents("./counter.txt", ++$_SESSION['c']);
+}
+?>
+<html>
+<head />
+	<title>I will Google before asking dumb questions</title>
+<body style="background-image:url('google1.jpg');background-size:contain" >
+	<span>Don't worry, you are the <?= $_SESSION['c'] ?><sup>th</sup> person.</span>
+<body>
+</html>
